f3e72fa768596143c6b1260992842e3391beaa50
Zaw Moe Htike <uchihamoee@gmail.com>
2024-03-23 23:37:46 +0700

    handlers for api call

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -24,6 +24,8 @@ func main() {
 
 	log.Println("Starting application on port", port)
 
+	http.HandleFunc("/", Hello)
+
 	// start a web server
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
