f88776861aada79316e3b3504eec3b2e9d1becbe
Zaw Moe Htike <uchihamoee@gmail.com>
2024-03-24 21:51:06 +0700

    repo

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -30,6 +30,7 @@ func main() {
 		log.Fatal(err)
 	}
 	app.DB = conn
+	defer app.DB.Close()
 
 	app.Domain = "example.com"
 
---
+++ internal/repository/repository.go
@@ -0,0 +1,8 @@
+package repository
+
+import "backend/internal/models"
+
+
+type DatabaseRepo interface {
+	AllMovies() ([]*models.Movie, error)
+}
