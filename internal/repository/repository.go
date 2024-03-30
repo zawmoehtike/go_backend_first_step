f2dba07e287fcfdac6fb97a724f3e91d87c2d9f9
Zaw Moe Htike <uchihamoee@gmail.com>
2024-03-30 15:16:27 +0700

    move details, edit (data layer)

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -11,4 +11,7 @@ type DatabaseRepo interface {
 	AllMovies() ([]*models.Movie, error)
 	GetUserByEmail(email string) (*models.User, error)
 	GetUserByID(id int) (*models.User, error)
+
+	OneMovieForEdit(id int) (*models.Movie, []*models.Genre, error)
+	OneMovie(id int) (*models.Movie, error)
 }
